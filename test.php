b255bc30d0261b67ad5e0cec6de60856c98ddb15
kokoemon <kokoemon>
2020-04-22 08:51:03 +0900

    make_eventにmodelをpullした

--- test.php
+++ test.php
@@ -3,8 +3,8 @@
 
 include( "./test_access_db.php" );
 
-#regist_event('誕生日会','みんなでお祝いしましょう','zxmkjhfaskjh');
-get_event(1);
+regist_event('誕生日会','みんなでお祝いしましょう','zxmkjhfaskjh');
+get_event(3);
 
 echo $global_event_name;
 
