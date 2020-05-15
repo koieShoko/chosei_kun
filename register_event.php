370bd9da1768d96a5a0606923797a3adb717aa26
kokoemon <kokoemon>
2020-05-15 14:39:29 +0900

    入力画面の説明文の追加

--- register_event.php
+++ register_event.php
@@ -46,6 +46,7 @@ session_start();
   $dates = html($dates);
   $dates = str_replace(['/r/n'|'/r'|'\n'], PHP_EOL, $dates);
   $dates = explode(PHP_EOL, $dates);
+  //特殊文字を削除
   $dates = delete_sc_dates($dates);
   //配列の空白要素を削除
   $dates = ary_empty_delete($dates);
@@ -83,68 +84,5 @@ session_start();
     header( "Location: ./event_url.php");
   }
 
-
-// //POSTされたかを確認
-// if($_SERVER["REQUEST_METHOD"]==="POST"){
-//   $name = "";
-//   $explain = "";
-//   $lines = array(); //候補日チェック前
-//
-//   //イベント名を取得
-//   $name = $_POST['event_name'];
-//   //イベント概要を取得
-//   $explain = $_POST['free'];
-//   //dateをPOSTで受け取り,開業区切りで配列に格納
-//   $dates = $_POST['event_dates'];
-//   $dates = html($dates);
-//   $dates = str_replace(['/r/n'|'/r'|'\n'], PHP_EOL, $dates);
-//   $dates = explode(PHP_EOL, $dates);
-//
-//   // var_dump($dates);
-//   // echo '<br>';
-//   /////////////   イベント名チェック   /////////////
-//   if( $name == '' ){ //文字が入力されているかチェック
-//     $_SESSION[ 'message' ] = "<h4>イベント名が入力されていません</h4>";
-//     header( "Location: ./dialog.php");
-//
-//   }else{
-//     $name = html($name);
-//     $name = delete_sc_event($name);
-//     if(mb_strlen($name) > 120 ){
-//       $_SESSION[ 'messaege' ] = "<h4>イベント名の文字数がオーバーしています</h4>";
-//       header( "Location: ./dialog.php");
-//     }
-//   }
-//
-//   /////////////   イベント名概要   /////////////
-//   if(!(empty($explain))){  //入力されているかチェック
-//     $explain = html($explain);
-//     $explain = delete_sc_event($explain);
-//     if(mb_strlen($explain) > 300){ //文字数が300文字以上入力
-//       $_SESSION[ 'message' ] = "<h4>イベント概要の文字数がオーバーしています。</h4>";
-//       header( "Location: ./dialog.php");
-//     }
-//   }
-//   ////////////   イベント候補日概要  /////////////
-//   if(count($dates) == 0){ //少なくとも候補日が1つ入力されているか
-//     $_SESSION[ 'message' ] = "<h4>候補日が入力されていません。</h4>";
-//     header( "Location: ./dialog.php");
-//   }else if(count($dates) > 30){ //候補日を20以上受け付けられるか
-//     $_SESSION[ 'message' ] = "<h4>入力した候補日が多すぎます。</h4>";
-//     header( "Location: ./dialog.php");
-//   }else{
-//     $dates = delete_sc_dates($dates);
-//     //配列の空白要素を削除
-//     $dates = ary_empty_delete($dates);
-//     // var_dump($dates);
-//   }
-//
-//   // for($i=0; $i<count($lines); $i++) {
-//   //   exe_regist_event_and_event_date($name,$explain, $lines);
-//   // }
-// }else{
-//
-//   $_SESSION[ 'message' ] = "<h4>不正なアクセスです。</h4>";
-// }
 
 ?>
